f6105c4b986b343392dda2dc26421ac26aa55b5d
Kjetil Hauger <kjetil.h.hauger@gmail.com>
2024-02-15 12:03:49 +0100

    fixed sorting in filter with pricing and got product page up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const content = document.getElementById("content");
-const men = document.getElementById("filterMen");
 const apiUrl = 'https://api.noroff.dev/api/v1/square-eyes';
 const allFilter = document.getElementById("all");
 const actionFilter = document.getElementById("action");
@@ -7,7 +6,69 @@ const comedyFilter = document.getElementById("comedy");
 const dramaFilter = document.getElementById("drama");
 const horrorFilter = document.getElementById("horror");
 const kidsFilter = document.getElementById("kids");
-const cardBtn = document.getElementById("card");
+const movieCard = document.getElementById("card");
+
+let apiData = [];
+
+async function fetchApi() {
+    try {
+        const response = await fetch(apiUrl);
+        const dataResult = await response.json();
+        apiData = dataResult;
+        for (let i = 0; i < apiData.length; i++) {
+            let data = apiData[i];
+            if (data.onSale === true) {
+                content.innerHTML += `
+                    <a href="movie.html?${data.id}"
+                        <div id="card">
+                            <img src="${data.image}" alt="Image of ${data.title}">
+                            <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                            </div>
+                        </div>
+                    </a>
+                `;
+            } else {
+                content.innerHTML += `
+                <a href="movie.html?${data.id}"
+                        <div id="card">
+                            <img src="${data.image}" alt="Image of ${data.title}">
+                            <div id="price">
+                            <span class="price">${data.price}</span>
+                            </div>
+                        </div>
+                    </a>
+                `;
+            }
+        }
+    } catch (error) {
+        renderError();
+    }
+}
+
+function renderError() {
+    const error = document.getElementById("content");
+    error.innerHTML = "ERROR, could not load API data. Please reload";
+}
+
+fetchApi();
+
+function dropdownFilterFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(dropDownClick) {
+    if (!dropDownClick.target.matches('#filterButton')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content")
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
 
 allFilter.addEventListener("click", () => {
     filterAll("All");
@@ -28,88 +89,6 @@ kidsFilter.addEventListener("click", () => {
     filterKids("Kids");
 });
 
-
-
-let apiData = [];
-
-// fetch(apiUrl)
-// .then((response) => response.json())
-// .catch(() => renderError())
-// .then((dataResult) => {
-//     apiData = dataResult;
-//     for (let i = 0; 0 < apiData.length; i++) {
-//         let data = apiData[i];
-//             if (data.onSale === true) {
-//                 content.innerHTML += `
-//                     <div class="card">
-//                         <img src="${data.image}" alt="Image of ${data.title}">
-//                         <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
-//                         </div>
-//         `
-//             } else {
-//                 content.innerHTML += `
-//                     <div class="card">
-//                         <img src="${data.image}" alt="Image of ${data.title}">
-//                     </div>
-//         `
-//             }
-//         }
-// });
-
-// function renderError() {
-//     const error = document.getElementById("content")
-//     error.innerHTML = "ERROR"
-// }
-
-async function fetchApi() {
-    try {
-        const response = await fetch(apiUrl);
-        const dataResult = await response.json();
-        apiData = dataResult;
-        for (let i = 0; i < apiData.length; i++) {
-            let data = apiData[i];
-            if (data.onSale === true) {
-                content.innerHTML += `
-                    <div id="card">
-                        <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
-                    </div>
-                `;
-            } else {
-                content.innerHTML += `
-                    <div id="card">
-                        <img src="${data.image}" alt="Image of ${data.title}">
-                    </div>
-                `;
-            }
-        }
-    } catch (error) {
-        renderError();
-    }
-}
-
-function renderError() {
-    const error = document.getElementById("content");
-    error.innerHTML = "ERROR, could not load API data. Please reload";
-}
-
-fetchApi();
-
-function dropdownFilterFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('#filterButton')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content")
-        for (let i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 function filterAll() {
     content.innerHTML = "";
     fetchApi()
@@ -120,14 +99,29 @@ function filterAction(genreToFilterBy) {
     for (const data of apiData) {
         if (data.genre === genreToFilterBy) {
             actionResults.push(data);
-        } if (data.genre === "Action"){
+        } if (data.genre === "Action" && data.onSale === false){
             content.innerHTML += `
+            <a href="movie.html?${data.id}"
+                <div id="card">
+                    <img src="${data.image}" alt="Image of ${data.title}">
+                    <div id="price">
+                    <span class="price">${data.price}</span>
+                    </div>
+                </div>
+            </a>
+                `;
+        } else if (data.genre === "Action" && data.onSale === true) {
+            content.innerHTML +=`
+                <a href="movie.html?${data.id}"
                     <div id="card">
                         <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
+                        <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                        </div>
                     </div>
-                `;
-        }
+                </a>`
+        } 
     } 
 }
 function filterComedy(genreToFilterBy) {
@@ -136,14 +130,29 @@ function filterComedy(genreToFilterBy) {
     for (const data of apiData) {
         if (data.genre === genreToFilterBy) {
             comedyResults.push(data);
-        } if (data.genre === "Comedy"){
+        } if (data.genre === "Comedy" && data.onSale === false){
             content.innerHTML += `
+            <a href="movie.html?${data.id}"
+                <div id="card">
+                    <img src="${data.image}" alt="Image of ${data.title}">
+                    <div id="price">
+                    <span class="price">${data.price}</span>
+                    </div>
+                </div>
+            </a>
+                `;
+        } else if (data.genre === "Comedy" && data.onSale === true) {
+            content.innerHTML +=`
+                <a href="movie.html?${data.id}"
                     <div id="card">
                         <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
+                        <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                        </div>
                     </div>
-                `;
-        }
+                </a>`;
+        } 
     } 
 }
 function filterDrama(genreToFilterBy) {
@@ -152,14 +161,29 @@ function filterDrama(genreToFilterBy) {
     for (const data of apiData) {
         if (data.genre === genreToFilterBy) {
             dramaResults.push(data);
-        } if (data.genre === "Drama"){
+        } if (data.genre === "Drama" && data.onSale === true){
             content.innerHTML += `
+            <a href="movie.html?${data.id}"
+                <div id="card">
+                    <img src="${data.image}" alt="Image of ${data.title}">
+                    <div id="price">
+                    <span class="price">${data.price}</span>
+                    </div>
+                </div>
+            </a>
+                `;
+        } else if (data.genre === "Drama" && data.onSale === true) {
+            content.innerHTML +=`
+                <a href="movie.html?${data.id}"
                     <div id="card">
                         <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
+                        <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                        </div>
                     </div>
-                `;
-        }
+                </a>`
+        } 
     } 
 }
 function filterHorror(genreToFilterBy) {
@@ -168,14 +192,29 @@ function filterHorror(genreToFilterBy) {
     for (const data of apiData) {
         if (data.genre === genreToFilterBy) {
             horrorResults.push(data);
-        } if (data.genre === "Horror"){
+        } if (data.genre === "Horror" && data.onSale === false){
             content.innerHTML += `
+            <a href="movie.html?${data.id}"
+                <div id="card">
+                    <img src="${data.image}" alt="Image of ${data.title}">
+                    <div id="price">
+                    <span class="price">${data.price}</span>
+                    </div>
+                </div>
+            </a>
+                `;
+        } else if (data.genre === "Horror" && data.onSale === true) {
+            content.innerHTML +=`
+                <a href="movie.html?${data.id}"
                     <div id="card">
                         <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
+                        <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                        </div>
                     </div>
-                `;
-        }
+                </a>`
+        } 
     } 
 }
 function filterKids(genreToFilterBy) {
@@ -184,21 +223,28 @@ function filterKids(genreToFilterBy) {
     for (const data of apiData) {
         if (data.genre === genreToFilterBy) {
             kidsResults.push(data);
-        } if (data.genre === "Kids"){
+        } if (data.genre === "Kids" && data.onSale === false){
             content.innerHTML += `
+            <a href="movie.html?${data.id}"
+                <div id="card">
+                    <img src="${data.image}" alt="Image of ${data.title}">
+                    <div id="price">
+                    <span class="price">${data.price}</span>
+                    </div>
+                </div>
+            </a>
+                `;
+        } else if (data.genre === "Kids" && data.onSale === true) {
+            content.innerHTML +=`
+                <a href="movie.html?${data.id}"
                     <div id="card">
                         <img src="${data.image}" alt="Image of ${data.title}">
-                        <img src="assets/images/sale.webp" id="saleImg" alt="Sale tag">
+                        <div id="price">
+                            <span class="price">${data.price}</span>
+                            <span class="discounted">${data.discountedPrice}</span>
+                        </div>
                     </div>
-                `;
+                </a>`
         }
     } 
 }
-
-// cardBtn.addEventListener("click", () => {
-//     onCardClick();
-// });
-
-// function onCardClick() {
-//     window.location.replace(`movie.html/?id=data.id`)
-// }
